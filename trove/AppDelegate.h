972c14a19f8234c52506cf71550804f0ee16af4f
Michael Pfister <pfista@gmail.com>
2014-02-14 23:54:46 -0600

    Initial Commit

---
+++ trove/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  trove
+//
+//  Created by Michael Pfister on 2/14/14.
+//  Copyright (c) 2014 Michael Pfister. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ trove/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  trove
+//
+//  Created by Michael Pfister on 2/14/14.
+//  Copyright (c) 2014 Michael Pfister. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
